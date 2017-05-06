f768b03727ac964c919909151242e6e5516a4b6c
Ajay Arjun <arjun.bka@gmail.com>
2017-05-06 12:29:39 -0500

    modifications for heroku

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var express = require('express');
 // create the app
 
 var app = express();
+const port = process.env.PORT || 5000;
+
+app.use(function(req,res,next){
+  if(req.heaeders['x-forwarded-proto']==='http'){
+    next();
+  }
+  else{
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(5000,function()
+app.listen(port,function()
 {
-  console.log('listening on port 5000');
+  console.log('listening on port' + port);
 });
