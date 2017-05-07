c22ba68abbc8f32efc49446514657006e847c4a7
Ajay Arjun <arjun.bka@gmail.com>
2017-05-06 20:58:12 -0500

    Styling Examples page and Main

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,22 +1,40 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-// var Examples = React.createClass({
-//   render:function()
-//   {
-//     return(
-//       <h3>Example</h3>
-//     );
-//   }
-// })
+var Examples = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li>
+            <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+          </li>
+          <li>
+            <Link to='/?location=Rio'>Rio, Brazil</Link>
+          </li>
+        </ol>
+      </div>
+    );
+  }
+})
 
-
-
-var Examples = ()=>{
-  return(
-    <h3>Example</h3>
-  );
-  };
-
-
+// var Examples = ()=>{
+//   return(
+//     <div>
+//     <h1 className="text-center">Example</h1>
+//     <p> Here are few example locations to try out</p>
+//     <ol>
+//       <li>
+//         <Link to="/?location=LosAngeles">Los Angeles</Link>
+//       </li>
+//       <li>
+//         <Link to="/?location=San Jose">San Jose</Link>
+//       </li>
+//     </ol>
+//   </div>
+//   );
+//   };
 
 module.exports = Examples;
