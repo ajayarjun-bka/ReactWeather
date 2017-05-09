6472a282af485dadc3b9294184f442d91feab9e8
Ajay Arjun <arjun.bka@gmail.com>
2017-05-09 14:47:05 -0500

    Fixed deg symbol

--- app/components/WeatherResult.jsx
+++ app/components/WeatherResult.jsx
@@ -3,7 +3,7 @@ var React  = require('react');
 var WeatherResult= ({temperature, location}) =>{
     return(
       <div>
-      <h3 className="text-center">It's {temperature} in {location}</h3>
+      <h3 className="text-center">It's {temperature} &deg;F in {location}</h3>
       </div>
     )
   };
